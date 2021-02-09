62fb4a91c06a9cdbb56c68abddaf985931a77635
Amonteverde04 <amonteverde98@gmail.com>
2021-02-09 16:37:39 -0500

    Adding assignment 1
    Submission for assignment 1

---
+++ Assignment1_Monteverde_Alex_BCS345/src/assignment1_monteverde_alex_bcs345/Assignment1_Monteverde_Alex_BCS345.java
@@ -0,0 +1,20 @@
+
+package assignment1_monteverde_alex_bcs345;
+
+
+public class Assignment1_Monteverde_Alex_BCS345 {
+
+    
+    public static void main(String[] args) {
+        // declare variable 'x' as a integer data type and set it equal to 0.
+        int x = 0;
+        
+        // Looping 0 thru 100, even numbers only.
+        while(x <= 100) // while x <= 100... print x, then add 2 to x.
+        {
+            System.out.println(x);
+            x = x + 2;
+        } // exits while loop onces condition is no longer true.
+    }
+    
+}
